3f080091c6b96aafe00b40d87d74a549ffd0510f
Miracle Obiano <68954478+Miracoll@users.noreply.github.com>
2020-07-31 14:21:10 +0100

    Add files via upload

---
+++ Blinking.c
@@ -0,0 +1,20 @@
+/*
+ * File:   Blinking.c
+ * Author: Miracol Obiano
+ *
+ * Created on July 31, 2020, 11:13 AM
+ */
+
+
+#include <xc.h>
+#include "config.c"
+
+void main(void) {
+    int value = 0;
+    TRISBbits.TRISB1=0;
+    RB1=0;
+    while(1){
+        RB1 = ~RB1;
+        __delay_ms(1000);
+    }
+}
